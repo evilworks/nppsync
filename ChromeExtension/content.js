1ddbbc804d61306bc84d3aabed6d8b3d43157e66
evilworks <pollux@lavabit.com>
2013-02-19 19:18:50 +0100

    Introduced content.js to pull DOM from a page.

---
+++ ChromeExtension/content.js
@@ -0,0 +1,29 @@
+// This file is part of NppSync plugin by evilworks.
+// Version: 1.1.0
+// Licence: Public Domain.
+
+function getLocalLinks(h) {
+  var r = [];
+  for (i=0; i<h.length;i++) {
+    if (h[i].href) {
+      if (h[i].href.indexOf('file:///') > -1) {
+        r[r.length] = h[i].href.split('///')[1];
+      }
+    }
+    if (h[i].src) {
+      if (h[i].src.indexOf('file:///') > -1) {
+        r[r.length] = h[i].src.split('///')[1];
+      }
+    }
+  }
+  return r
+}
+
+function messageListener(message, sender, sendResponse) {
+  if (message == 'getResources') { 
+    sendResponse(getLocalLinks(document.styleSheets).concat(getLocalLinks(document.scripts)));
+  };
+  return true;
+};
+
+chrome.extension.onMessage.addListener(messageListener);
